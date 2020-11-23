1bd3ac5257d54e86c9f7dc0da8bb89c701f24b0d
opilane <opilane@HP-CZC6297B82>
2020-11-23 17:48:59 +0200

    лопеывкн

--- temperament/temperament/MainPage.xaml.cs
+++ temperament/temperament/MainPage.xaml.cs
@@ -7,121 +7,114 @@ using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
-namespace temperament
+namespace Temperament
 {
     public partial class MainPage : CarouselPage
     {
-        private object Children;
-
+        Button btn;
+        Uri ur;
         public MainPage()
         {
+
+            btn = new Button { Text = "Пройти тест", BackgroundColor = Color.LightBlue, HorizontalOptions = LayoutOptions.Center };
+            Button btn1 = new Button { Text = "Пройти тест", BackgroundColor = Color.LightBlue, HorizontalOptions = LayoutOptions.Center };
+            Button btn2 = new Button { Text = "Пройти тест", BackgroundColor = Color.LightBlue, HorizontalOptions = LayoutOptions.Center };
+            Button btn3 = new Button { Text = "Пройти тест", BackgroundColor = Color.LightBlue, HorizontalOptions = LayoutOptions.Center };
+
+            btn.Clicked += Btn_Clicked;
+            btn1.Clicked += Btn1_Clicked;
+            btn2.Clicked += Btn2_Clicked; btn3.Clicked += Btn3_Clicked;
+
+            var holerikcontentpage = new ContentPage
             {
-                Button btn;
-                Uri ur;
+
+                Content = new StackLayout
                 {
 
-                    btn = new Button { Text = "Пройти тест", BackgroundColor = Color.LightBlue, HorizontalOptions = LayoutOptions.Center };
-                    Button btn1 = new Button { Text = "Пройти тест", BackgroundColor = Color.LightBlue, HorizontalOptions = LayoutOptions.Center };
-                    Button btn2 = new Button { Text = "Пройти тест", BackgroundColor = Color.LightBlue, HorizontalOptions = LayoutOptions.Center };
-                    Button btn3 = new Button { Text = "Пройти тест", BackgroundColor = Color.LightBlue, HorizontalOptions = LayoutOptions.Center };
-
-                    btn.Clicked += Btn_Clicked;
-                    btn1.Clicked += Btn1_Clicked;
-                    btn2.Clicked += Btn2_Clicked; btn3.Clicked += Btn3_Clicked;
-
-                    var holerikcontentpage = new ContentPage
-                    {
-
-                        Content = new StackLayout
-                        {
-
-                            Children =
+                    Children =
                     {
                         new Label{Text="Холерик", BackgroundColor= Color.Cornsilk, HorizontalTextAlignment= TextAlignment.Center, FontSize=20},
-                        new Image{Source = "hol.jpg", },
+                        new Image{Source = "temp.jpg", },
                         new Label{Text="Холерик — быстрый, страстный, порывистый, однако совершенно неуравновешенный, с резко меняющимся настроением с эмоциональными вспышками, быстро истощаемый. У него нет равновесия нервных процессов, это его резко отличает от сангвиника. Холерик, увлекаясь, безалаберно растрачивает свои силы и быстро истощается."},
+
 
                         btn, }
 
 
-                        }
 
-                    };
-                    var flegmatikcontentpage = new ContentPage
-                    {
-                        Content = new StackLayout
-                        {
-                            Children =
+
+                }
+
+            };
+            var flegmatikcontentpage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
                     {
                         new Label{Text="Флегматик", BackgroundColor= Color.Cornsilk, HorizontalTextAlignment= TextAlignment.Center, FontSize=20},
-                        new Image{Source = "fle.jpg", },
+                        new Image{Source = "temp.jpg", },
                         new Label{Text="Флегматик неспешен, невозмутим, имеет устойчивые стремления и настроение, внешне скуп на проявление эмоций и чувств. Он проявляет упорство и настойчивость в работе, оставаясь спокойным и уравновешенным. В работе он производителен, компенсируя свою неспешность прилежанием."},
-
-                        
+                        btn1
                     }
-                        }
-                    };
-                    var sangvinikcontentpage = new ContentPage
-                    {
-                        Content = new StackLayout
-                        {
-                            Children =
+                }
+            };
+            var sangvinikcontentpage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
                     {
                         new Label{Text="Сангвиник", BackgroundColor= Color.Cornsilk, HorizontalTextAlignment= TextAlignment.Center, FontSize=20},
-                        new Image{Source = "san.jpg",},
+                        new Image{Source = "temp.jpg", },
                         new Label{Text="Сангвиник — живой, горячий, подвижный человек, с частой сменой настроения, впечатлений, с быстрой реакцией на все события, происходящие вокруг него, довольно легко примиряющийся со своими неудачами и неприятностями. Обычно сангвиники обладают выразительной мимикой. Он очень продуктивен в работе, когда ему интересно, приходя в сильное возбуждение от этого, если работа не интересна, он относится к ней безразлично, ему становится скучно."},
-
 
                         btn2
                     }
-                        }
-                    };
-                    var melanholikcontentpage = new ContentPage
-                    {
-                        Content = new StackLayout
-                        {
-                            Children =
+                }
+            };
+            var melanholikcontentpage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
                     {
                        new Label{Text="Меланхолик", BackgroundColor= Color.Cornsilk, HorizontalTextAlignment= TextAlignment.Center, FontSize=20},
-                       new Image{Source = "mel.JPG", },
-                       new Label{Text="Меланхолик — человек легко ранимый, склонный к постоянному переживанию различных событий, он мало реагирует на внешние факторы. Свои астенические переживания он не может сдерживать усилием воли, он чересчур впечатлителен, легко эмоционально раним."},
+                       new Image{Source = "temp.jpg", },
+                        new Label{Text="Меланхолик — человек легко ранимый, склонный к постоянному переживанию различных событий, он мало реагирует на внешние факторы. Свои астенические переживания он не может сдерживать усилием воли, он чересчур впечатлителен, легко эмоционально раним."},
 
                         btn3
                 }
-                        }
-                    };
-
-                    Children.Add(melanholikcontentpage);
-                    Children.Add(flegmatikcontentpage);
-                    Children.Add(holerikcontentpage);
-                    Children.Add(sangvinikcontentpage);
                 }
-
-                private async void Btn3_Clicked(object sender, EventArgs e)
-                {
-                    ur = new Uri("https://testometrika.com/personality-and-temper/the-formula-of-temperament-a-belov/");
-                    await Browser.OpenAsync(ur);
-                }
-
-                private async void Btn2_Clicked(object sender, EventArgs e)
-                {
-                    ur = new Uri("https://testometrika.com/personality-and-temper/the-formula-of-temperament-a-belov/");
-                    await Browser.OpenAsync(ur);
-                }
-
-                private async void Btn1_Clicked(object sender, EventArgs e)
-                {
-                    ur = new Uri("https://testometrika.com/personality-and-temper/the-formula-of-temperament-a-belov/");
-                    await Browser.OpenAsync(ur);
-                }
-
-                private async void Btn_Clicked(object sender, EventArgs e)
-                {
-                    ur = new Uri("https://testometrika.com/personality-and-temper/the-formula-of-temperament-a-belov/");
-                    await Browser.OpenAsync(ur);
-                }
-                
-                }
-            }
+            };
+            Children.Add(melanholikcontentpage);
+            Children.Add(flegmatikcontentpage);
+            Children.Add(holerikcontentpage);
+            Children.Add(sangvinikcontentpage);
         }
+
+        private async void Btn3_Clicked(object sender, EventArgs e)
+        {
+            ur = new Uri("https://testometrika.com/personality-and-temper/the-formula-of-temperament-a-belov/");
+            await Browser.OpenAsync(ur);
+        }
+
+        private async void Btn2_Clicked(object sender, EventArgs e)
+        {
+            ur = new Uri("https://brainapps.ru/test/temperament");
+            await Browser.OpenAsync(ur);
+        }
+
+        private async void Btn1_Clicked(object sender, EventArgs e)
+        {
+            ur = new Uri("https://moeobrazovanie.ru/prof_tests/test_temperament_i_professija.html");
+            await Browser.OpenAsync(ur);
+        }
+
+        private async void Btn_Clicked(object sender, EventArgs e)
+        {
+            ur = new Uri("https://www.psychologies.ru/tests/test/875/");
+            await Browser.OpenAsync(ur);
+        }
+    }
 }
